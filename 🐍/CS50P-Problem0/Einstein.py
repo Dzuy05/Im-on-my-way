67eb0d50b033091a32f30d8d927224af315b514d
Dzuy05 <duybp05@gmail.com>
2024-07-29 14:34:34 +0700

    Einsteinnn

---
+++ 🐍/CS50P-Problem0/Einstein.py
@@ -0,0 +1,7 @@
+#Square of the speed of light 
+c = 300000000**2
+#Mass of the object
+m = int(input("Enter the mass of the object: "))
+#Energy of the object
+e = m*c
+print (f"The energy of the object is {e} joules")
