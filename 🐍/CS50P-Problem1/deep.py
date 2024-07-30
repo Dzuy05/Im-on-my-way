25c94ff47d91248a52c587ea2c5a19639b0eb340
Dzuy05 <duybp05@gmail.com>
2024-07-30 22:27:02 +0700

    Another one

---
+++ 🐍/CS50P-Problem1/deep.py
@@ -0,0 +1,5 @@
+i = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").strip().lower()
+if i == "forty-two" or i == "forty two" or i == "42":
+    print("Yes")
+else:
+    print("No")
