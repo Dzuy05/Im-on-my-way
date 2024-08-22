0fc5b6115aaecd31340d8986a35890c1676c7cc6
Dzuy05 <duybp05@gmail.com>
2024-08-22 16:13:47 +0700

    Just change a little bit

--- 🐍/CS50P-Problem2/coke.py
+++ 🐍/CS50P-Problem2/coke.py
@@ -1,13 +1,17 @@
 c = int(50)
+total = 0
 print(f"Amount due: {c}")
-ls = [50 , 25, 5]
+ls = [10 , 25, 5]
 while c > 0:
     coin = int(input("Insert coin: "))
     if coin in ls:
         c -= coin
-        print(f"Amout due: {c}")
-    else:
-        coin = int(input("Insert coin: "))
+        total += coin
+        if c >= 0:
+            print(f"Amout due: {c}")
+        elif c < 0:
+            print(f"Change owed: {total - 50}")
+
     
 
         
